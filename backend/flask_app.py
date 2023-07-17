450d6543ef39d33ffe3e7d3bd40be869b0f6f7f7
YoavIlan <yoav@Yoavs-Laptop.lan>
2023-07-16 20:54:53 -0500

    post user API implemented

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import firebase_admin
+from firebase_admin import credentials, firestore
+from flask import Blueprint, request, jsonify
+import uuid
 
 app = Flask(__name__)
 
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
+
+
+cred = credentials.Certificate("apad-summer-23-firebase-adminsdk-l6p99-ed2d97db75.json")
+fb_app = firebase_admin.initialize_app(cred)
 
 #users route
 @app.route("/users")
@@ -17,6 +22,19 @@ def users():
               'beepbop@gmail.com': 'password'  
             }]
     }
+    
+db = firestore.client()
+users_ref = db.collection('users')
+userAPI = Blueprint('userAPI', __name__)
+
+@app.route('/adduser', methods=['POST'])
+def create_user():
+    try:
+        id = uuid.uuid4()
+        users_ref.document(id.hex).set(request.json)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An error occurred: {e}"
     
 if __name__ == '__main__':
     app.run(debug=True)
