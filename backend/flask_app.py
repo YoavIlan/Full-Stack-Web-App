68b6a76fd650b68c2f9621215efd43bf80b218f9
YoavIlan <yoav@wireless-10-145-50-130.public.utexas.edu>
2023-07-18 17:00:58 -0500

    ensured emails are unique and made alternative get user

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -16,16 +16,21 @@ resource_collection = db['resources']
 def create_user():
     try:
         # get email and password from json payload
-        email = next(iter(request.json))
-        password = request.json[email]
+        # email = next(iter(request.json))
+        # password = request.json[email]
+        email = request.json['email']
+        password = request.json['password']
+        print(email)
+        print(password)
         # encrypt with uuid5 (SHA-1 encryption)
         email_encrypt = str(uuid.uuid5(uuid.NAMESPACE_URL, email))
         password_encrypt = str(uuid.uuid5(uuid.NAMESPACE_URL, password))
         # save to DB using email as id and password as data
-        users_collection.insert_one({'email': email_encrypt, 'password': password_encrypt})
+        users_collection.insert_one({'_id': email_encrypt, 'password': password_encrypt})
         return jsonify({"success": True}), 200
     except Exception as e:
-        return f"An error occurred: {e}"
+        return jsonify({"success": False,
+                        "error": "User already taken!"}), 200
 
 # get a user and check for email and password matching
 @app.route('/api/getuser/<email>/<password>', methods=['GET'])
@@ -36,7 +41,29 @@ def get_user(email, password):
         password_encrypt = str(uuid.uuid5(uuid.NAMESPACE_URL, password))
         
         # get the document with the email id
-        result = users_collection.find_one({'email': email_encrypt, 'password': password_encrypt})
+        result = users_collection.find_one({'_id': email_encrypt, 'password': password_encrypt})
+        # no user found
+        if result is None:
+            return jsonify({"success": False,
+                            "message": 'Wrong username or password'}), 200
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An error occurred: {e}"
+    
+# testing with payload instead of with request url
+@app.route('/api/v2/getuser', methods=['GET'])
+def get_user2():
+    try:
+        email = request.json['email']
+        password = request.json['password']
+        print(email)
+        print(password)
+        # encrypt email and password - this probably should come pre-encrypted to not send raw passwords over API
+        email_encrypt = str(uuid.uuid5(uuid.NAMESPACE_URL, email))
+        password_encrypt = str(uuid.uuid5(uuid.NAMESPACE_URL, password))
+        
+        # get the document with the email id
+        result = users_collection.find_one({'_id': email_encrypt, 'password': password_encrypt})
         # no user found
         if result is None:
             return jsonify({"success": False,
