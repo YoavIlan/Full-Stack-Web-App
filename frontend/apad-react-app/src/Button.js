307baefe5c2d94119016b76f10590517364c2c8e
Joel Brisola <joel_brisola@utexas.edu>
2023-07-22 12:49:08 -0500

    Adding Generic Button

---
+++ frontend/apad-react-app/src/Button.js
@@ -0,0 +1,14 @@
+const Button = () => {
+
+    const handleClick = () => {
+        /* insert desired button clicked behavior here */
+    }
+
+    return (
+        <button onClick={handleClick}>
+            insert button text here 
+            </button>
+    )
+}
+
+export default Button;
