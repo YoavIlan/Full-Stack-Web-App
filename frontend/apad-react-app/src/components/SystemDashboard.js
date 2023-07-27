e423badf32dc0a05dd0602637bff46abc41495b5
Hannah Clark <hannahlclarkk@utexas.edu>
2023-07-27 14:20:15 -0500

    added styling to system dashboard, still hardcoded values

--- frontend/apad-react-app/src/components/SystemDashboard.js
+++ frontend/apad-react-app/src/components/SystemDashboard.js
@@ -5,8 +5,12 @@ import Table from './Table';
 export const SystemDashboard = () => {
     return (
         <>
-        <h1>System Dashboard</h1>
-        <Table/>
+            <div className="App">
+                <header className="App-header">    
+                    <h1>System Dashboard</h1>
+                    <Table/> 
+                </header>
+            </div>
         </>
     )
 }
