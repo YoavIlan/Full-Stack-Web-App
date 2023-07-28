b762651ffad2883b3bcffd38d2e6e7c03a479b9d
Joel Brisola <joel_brisola@utexas.edu>
2023-07-28 18:01:44 -0500

    Added Backend Communication

--- frontend/apad-react-app/src/components/ProjectCreation.js
+++ frontend/apad-react-app/src/components/ProjectCreation.js
@@ -1,27 +1,100 @@
+import React, { useState } from "react";
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import '../App.css';
 import { MUIButton } from './MUIButtons';
-import {MUITextField} from './TextFields';
 import { MUIBox } from './MUIBox';
-import React, { useState } from "react";
-import Typography from '@mui/material/Typography';
+import APIService from './APIService'
+import Grid from '@mui/material/Grid';
+import CssBaseline from '@mui/material/CssBaseline';
 
 // Export the Project Creation component
 export const ProjectCreation = () => {
+  const[_id, setId] = useState('')
+  const[name, setName] = useState('')
+  const[description, setDescription] = useState('')
+  const [data, setData] = useState([{}])
+
+  //The add user function to call the create project api service from APIService class to process json data load
+  //And send to the mongodb
+  const createProject = () =>{
+      APIService.CreateProject({_id, name, description}).then(
+        data => {
+          setData(data)
+          console.log(data)
+          alert(data.message)
+        }
+      )
+      .catch(error => console.log('error', error))
+  }
+
+  //The handleSubmit will be triggered upon clicking the create project button
+  const handleSubmit = (event) => {
+    console.log(_id)
+    console.log(name)
+    console.log(description)
+    createProject()
+    setId('')
+    setName('')
+    setDescription('')
+};
 
   // Project Creation UI setup
     return (
       <div className="App">
+        <CssBaseline />
           <Typography component="h1" variant="h5">
             Create a New Project
           </Typography>
-          <MUIBox>
-          <MUITextField label="Project Name"/>
-          <MUITextField label="Project ID"/>
-          <MUITextField label="Project Description"/>
-          <MUIButton>
+          
+          <MUIBox component="form" noValidate sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+          <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="Project Name" 
+            label="Project Name"
+            name = "Project Name"
+            autoComplete="Project Name"
+            value={name}
+            onChange={(e) => {setName(e.target.value);console.log(e.target.value)}}
+            />
+            </Grid>
+            <Grid item xs={12}>
+           <TextField
+            required
+            fullWidth
+            id="Project ID" 
+            label="Project ID"
+            name = "Project ID"
+            autoComplete="Project ID"
+            value={_id}
+            onChange={(e) => {setId(e.target.value);console.log(e.target.value)}}
+            />
+            </Grid>
+            <Grid item xs={12}>
+           <TextField
+            required
+            fullWidth
+            id="Project Description" 
+            label="Project Description"
+            name = "Project Description"
+            autoComplete="Project Description"
+            value={description}
+            onChange={(e) => {setName(e.target.value);console.log(e.target.value)}}
+            />
+            </Grid>
+          </Grid>
+          <MUIButton
+          fullWidth
+          variant="contained"
+          onClick={() => { handleSubmit();}}
+          sx={{ mt: 3, mb: 2 }}>
             Create Project
           </MUIButton>
-          <MUIButton >
+          <MUIButton > 
+            {/* Need to add routing functionality here */}
             Cancel      
           </MUIButton>
           </MUIBox>
