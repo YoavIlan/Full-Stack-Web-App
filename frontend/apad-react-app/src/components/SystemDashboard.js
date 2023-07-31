bcb7388c82f0f18a1cf9a198afc25980d37285bc
Hannah Clark <hannahlclarkk@utexas.edu>
2023-07-31 15:02:20 -0500

    system dashboard updated and pretty with materials ui

--- frontend/apad-react-app/src/components/SystemDashboard.js
+++ frontend/apad-react-app/src/components/SystemDashboard.js
@@ -2,15 +2,12 @@ import '../App.css';
 import React, { useState} from "react";
 import Table from './Table';
 
+
 export const SystemDashboard = () => {
     return (
         <>
-            <div className="App">
-                <header className="App-header">    
-                    <h1>System Dashboard</h1>
-                    <Table/> 
-                </header>
-            </div>
+        <h1>System Dashboard</h1>
+        <Table/>  
         </>
     )
 }
--- frontend/apad-react-app/src/components/Table.js
+++ frontend/apad-react-app/src/components/Table.js
@@ -1,48 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Row = (props) => {
-    const {hwSet, capacity, availability} = props;
-    return(
-        <tr>
-            <td>{hwSet}</td>
-            <td>{capacity}</td>
-            <td>{availability}</td>
-        </tr>
-    ) 
-}
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-const Table = (props) => {
-    const rows = props.data;
-    return(
-        <table>
-            <thead>
-                <tr>
-                    <th className="table-header">HW Set</th>
-                    <th className="table-header">Capacity</th>
-                    <th className="table-header">Availability</th>
-                </tr>
-            </thead>
-            <tbody>
-                {rows.map((row, index) => (
-                <Row key={index} hwSet={row.hwSet} capacity={row.capacity} availability={row.availability} />
-                ))}
-            </tbody>
-        </table>
-    )
-}
-
-const tableList = [
-    { hwSet: 'bikes', capacity: '10', availability: '1'},
-    { hwSet: 'scooters',capacity: '10', availability: '8'},
-    { hwSet: 'drones',capacity: '10', availability: '5'}
-]
 const TableMain = () => {
-    const [ tableData, setTableData] = useState(tableList);
+    const [ data, setTableData] = useState([{}]);
+    useEffect(() => {
+        var address = "/api/getresources"
+        fetch(address)
+          .then(response => response.json())
+          .then(data =>setTableData(data))
+      }, []);
+    
     return(
         <>
-        <Table data={tableData} />
+        {/* <Table data={tableData} /> */}
+        <div>
+            {(typeof data.data === "undefined") ? (
+                <p>Loading...</p>
+            ): (
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                        <TableCell>Resource</TableCell>
+                        <TableCell align="right">Capacity</TableCell>
+                        <TableCell align="right">Availability</TableCell>
+                    </TableRow>
+                </TableHead>
+                    <TableBody>
+                        {data.data.map((resource) => (
+                            <TableRow
+                                key={data._id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="data">
+                                    {resource._id}
+                                </TableCell>
+                                <TableCell align="right">{resource.capacity}</TableCell>
+                                <TableCell align="right">{resource.availability}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            )}
+        </div>
         </>
     )
 }
+
 
 export default TableMain;
