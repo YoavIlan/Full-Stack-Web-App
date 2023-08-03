7cda00854020f265f01b2a0df4ef7a96b43f2cbf
Yoav Ilan <ilan.yoav@gmail.com>
2023-08-03 00:52:47 -0500

    check in and out code cleanup

--- frontend/apad-react-app/src/components/CheckInOut.js
+++ frontend/apad-react-app/src/components/CheckInOut.js
@@ -13,19 +13,25 @@ import {useLocation} from "react-router-dom";
 
 const CheckInOut = () => {
     const {state} = useLocation();
+    // project ID
     const[id, setId] = useState('')
+    // user's request to send to backend
     const[usrRequest, setUsrRequest] = useState([])
+    // project info
     const [projectInfo, setProjectInfo] = useState([{}])
+    // resources info
     const [resourceData, setResourceData] = useState([{}])
+    // merged object of resources and project to display
     const [mergedDictionary, setMergedDictionary] = useState([{}])
 
     useEffect(() => {
-        console.log(state.data.data)
+        // console.log(state.data.data)
         setId(state.data.data._id)
         setProjectInfo(state.data.data)
         combineResourceProjectData()
     }, [])
 
+    // force update state whenever usrRequest is updated
     useEffect(() => {
     }, [usrRequest])
 
@@ -35,11 +41,12 @@ const CheckInOut = () => {
             setResourceData(resourceData)
             APIService.AccessProject(state.data.data._id).then((projData) =>
             {
+                // data to display on table. capacity, availability, checked out
                 const combinedData = resourceData.data.map((resource) => ({
                     ...resource, checkedOut: projData.data.resources[resource._id]
                 }))
                 setMergedDictionary(combinedData)
-                console.log(combinedData)
+                // console.log(combinedData)
                 const emptyRequest = resourceData.data.reduce((acc, item) => {
                     acc[item._id] = 0
                     return acc
@@ -50,17 +57,19 @@ const CheckInOut = () => {
     }
 
     const handleChange = (e, resourceId) => {
-        const { value } = e.target;
+        const value = e.target.value;
+        // make copy to update
         const prev_copy = {...usrRequest}
+        // string to int, 0 if empty
         prev_copy[resourceId] = parseInt(value) || 0
         setUsrRequest(prev_copy)
       };
 
-    // TODO: checkObject needs to be {resource: numbertorequest, resource2: numbertorequest}
     const checkIn = (e) => {
         APIService.CheckIn({id, data: usrRequest}).then(
             info => {
               combineResourceProjectData()
+              // can only check in what you have
               if(!info.success)
                 alert(info.message)
             }
@@ -68,7 +77,6 @@ const CheckInOut = () => {
           .catch(error => console.log(error))
     }
 
-    // // Check Out function using the API Service and a check that adds additional resources as needed
     const checkOut = (e) => {
         APIService.CheckOut({id, data: usrRequest}).then(
             info => {
@@ -80,87 +88,54 @@ const CheckInOut = () => {
 
   return (
     <div>
-        {/* <Box
-        width= "550px"
-        height= "300px"
-        backgroundColor= "white"
-        hover="{
-        backgroundColor: 'primary.main'
-        opacity: [0.9, 0.8, 0.7]"
-        display={'block'}
-        style={{padding:"auto", margin:"auto", alignItems:"left", justifyContent:"center" }}
-        > */}
-        {/* <Grid container spacing={2} style={{padding:"5px", margin:"5px" }}>
-          <Grid item xs={8}>
-            <TextField
-              required
-              fullWidth
-              id="id"
-              label="Project ID"
-              name="id"
-              value={id}
-              onChange={(e) => {setId(e.target.value);console.log(e.target.value)}}
-              />
-          </Grid>
-          <Grid item xs={4}>
-            <MUIButton onClick={() => {accessProject()}}>
-              Access
-            </MUIButton>
-          </Grid>
-        </Grid> */}
-        {/* <h2> Resource Management</h2>
-        <h3> Selected Project: {projectName}</h3> */}
-                        {/* <MUIButton onClick={() => {accessProject()}}>
-              Access
-            </MUIButton> */}
-            {(typeof resourceData.data === "undefined" || typeof projectInfo === "undefined") ? (
-                <p>Loading...</p>
-            ): (
-            
-            <TableContainer component={Paper} style={{margin:"10px" }}>
-                <Table sx={{ minWidth: 6 }} aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                        <TableCell>Resource</TableCell>
-                        <TableCell align="center">Capacity</TableCell>
-                        <TableCell align="center">Availability</TableCell>
-                        <TableCell align="center">Checked Out</TableCell>
-                        <TableCell align="center">Request</TableCell>
-                    </TableRow>
-                </TableHead>
-                    <TableBody>
-                        {mergedDictionary.map((mapData) => (
-                            <TableRow
-                            key={mapData._id}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                            <TableCell align="center" component="th" scope="data">
-                                {mapData._id}
-                            </TableCell>
-                            <TableCell align="center">{mapData.capacity}</TableCell>
-                            <TableCell align="center">{mapData.availability}</TableCell>
-                            <TableCell align="center">{mapData.checkedOut}</TableCell>
-                            <TableCell align="center">
-                                <TextField
-                                    required
-                                    fullWidth
-                                    value={usrRequest[mapData._id]}
-                                    onChange={(e) => handleChange(e, mapData._id)}
-                                    />
-                            </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            )}
-            <MUIButton onClick={() => {checkIn()}}>
-                Check In
-            </MUIButton>
-            <MUIButton onClick={() => {checkOut()}}>
-                Check Out
-            </MUIButton> 
-        </div>
+        {(typeof resourceData.data === "undefined" || typeof projectInfo === "undefined") ? (
+            <p>Loading...</p>
+        ): (
+        
+        <TableContainer component={Paper} style={{margin:"10px" }}>
+            <Table sx={{ minWidth: 6 }} aria-label="simple table">
+                <TableHead>
+                <TableRow>
+                    <TableCell>Resource</TableCell>
+                    <TableCell align="center">Capacity</TableCell>
+                    <TableCell align="center">Availability</TableCell>
+                    <TableCell align="center">Checked Out</TableCell>
+                    <TableCell align="center">Request</TableCell>
+                </TableRow>
+            </TableHead>
+                <TableBody>
+                    {mergedDictionary.map((mapData) => (
+                        <TableRow
+                        key={mapData._id}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                        <TableCell align="center" component="th" scope="data">
+                            {mapData._id}
+                        </TableCell>
+                        <TableCell align="center">{mapData.capacity}</TableCell>
+                        <TableCell align="center">{mapData.availability}</TableCell>
+                        <TableCell align="center">{mapData.checkedOut}</TableCell>
+                        <TableCell align="center">
+                            <TextField
+                                required
+                                fullWidth
+                                value={usrRequest[mapData._id]}
+                                onChange={(e) => handleChange(e, mapData._id)}
+                                />
+                        </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+        )}
+        <MUIButton onClick={() => {checkIn()}}>
+            Check In
+        </MUIButton>
+        <MUIButton onClick={() => {checkOut()}}>
+            Check Out
+        </MUIButton> 
+    </div>
   )
 }
 
