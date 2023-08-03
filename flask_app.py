e963e51416317324508017c5deafe7ee0947e5c8
Yoav Ilan <ilan.yoav@gmail.com>
2023-08-03 12:04:10 -0500

    set app to public domain

--- flask_app.py
+++ flask_app.py
@@ -220,5 +220,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', debug=False, port=80)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', debug=False, port=80)
